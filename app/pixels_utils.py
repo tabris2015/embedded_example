3d25617cae5f65e992afe2baa9ef5d3c7706cf39
Jose Laruta <eduardo.laruta@gmail.com>
2023-12-12 08:29:55 -0400

    add disp pixels 2

--- app/pixels_utils.py
+++ app/pixels_utils.py
@@ -1,2 +1,18 @@
+
+
 def display_pixels(result_list):
-    print("display pixels!!!")
+    try:
+        import board
+        import neopixel_spi as neopixel
+        spi = board.SPI()
+        pixels = neopixel.NeoPixel_SPI(
+            spi,
+            8,
+            brightness=0.3,
+            auto_write=False,
+            pixel_order=neopixel.GRB,
+            bit0=0b10000000
+        )
+    except ImportError:
+        print("display pixels!!!")
+
